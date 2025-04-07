835694aee349544b3ea6ee64b48495377c1bcb73
kartheeksvyasa <kartheekkartheek49@gmail.com>
2025-04-07 11:42:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ function respondToUser (userMessage) {
 
     // Speech synthesis
     const utterance = new SpeechSynthesisUtterance(ghostResponse);
-    utterance.pitch = 1.5; // Adjust pitch for a ghostly effect
-    utterance.rate = 0.9; // Slower speed for a more eerie effect
+    utterance.pitch = 1.9; // Adjust pitch for a ghostly effect
+    utterance.rate = 1.2; // Slower speed for a more eerie effect
     utterance.voice = getGhostVoice(); // Get a ghostly voice
     window.speechSynthesis.speak(utterance);
 }
